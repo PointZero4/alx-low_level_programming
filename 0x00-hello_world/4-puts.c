836c4e6c2b3d549c0090083fb22f98da0974d4ea
Eric Asamoah-Boateng <asamoahboatengeric06@gmail.com>
2022-06-30 13:21:11 -0700

    she

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 
-
-
-/**
- *
- *  *  main - Entry point
- *
- *   *  Description: prints "\"Programming is like building a multilingual puzzle"
- *
- *    *  Return: 0
- *
- *     */
-
 int main(void)
 
 {
