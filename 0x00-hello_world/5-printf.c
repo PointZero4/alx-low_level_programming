b4e9ecfa570a7791e572433b770d39b9bb7694d0
Eric Asamoah-Boateng <asamoahboatengeric06@gmail.com>
2022-06-30 13:42:14 -0700

    j

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- 
- * main - Print a string
- 
- * Description: use printf
- 
- * Return: 0
- 
- */
+*
+* * main - Print a string
+*
+* * Description: use printf
+*
+* * Return: 0
+* 
+* */
 
 int main(void)
 
@@ -16,6 +16,6 @@ int main(void)
 
 		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+		return (0);
 
 }
