33c3c779ba7b8b45729444884ec64f228f2a5e4c
Eric Asamoah-Boateng <asamoahboatengeric06@gmail.com>
2022-07-05 14:59:31 -0700

    d

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,26 +5,32 @@
  * in lowercase, followed by a new line.
  * Return: Always 0.
  */
+
+
 void print_alphabet(void)
 
 {
 
-		char c = 'a';
+	char ch;
 
 
 
-		while (c <= 'z')
+	ch = 'a';
 
-		{
 
-			_putchar(c);
 
-	         	c++;
+	while (ch <= 'z')
 
-											}
+	{
 
-			_putchar('\n');
+		_putchar(ch);
 
-			
+		ch++;
+
+	}
+
+
+
+	_putchar('\n');
 
 }
