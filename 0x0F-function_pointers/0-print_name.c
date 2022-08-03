7cf2557b1f12abe3cce1e6f7cfbc23076df7cd1c
Eric Asamoah-Boateng <asamoahboatengeric06@gmail.com>
2022-08-03 08:07:22 -0700

    shed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,27 +1,23 @@
-#include <stdlib.h>
-
-#include "function_pointers.h"
-
-
-
 /**
  *
- *  * print_name - prints a name
+ *   *print_name - prints a name.
  *
- *   * @name: name to print
+ *     *@name: pointer to name.
  *
- *    * @f: pointer to the printing function
+ *       *@f: function pointer.
  *
- *     */
+ *         *
+ *
+ *           *Return: void.
+ *
+ *             */
 
 void print_name(char *name, void (*f)(char *))
 
 {
 
-		if (!name || !f)
+		if (name && f)
 
-					return;
-
-			f(name);
+					f(name);
 
 }
